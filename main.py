9c5ef91207116908ae102ae59499968fe302500f
alexxxmf <amoro@alumni.unav.es>
2018-03-16 16:28:21 +0000

    Adding more things to the block object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import hashlib
 
 
 class Block():
@@ -6,5 +7,13 @@ class Block():
     This is a simplified implementation of a bitcoin block
     model.
     """
-    def init(self, prev_hash, hash, data):
+    def init(self, data, prev_hash):
         self.timestamp = time.time()
+        self.prev_hash = prev_hash
+        self.current_hash = self.hash_block()
+        self.data = data
+
+    def hash_block(self):
+        concatenation = str(self.prev_hash) + str(self.data) + str(self.timestamp)
+        hash = hashlib.sha256(concatenation).hexdigest()
+        return hash
