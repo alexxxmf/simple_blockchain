4f36641f40edd6df3c3f2643ca9b7fcab10bd1ac
alexxxmf <amoro@alumni.unav.es>
2018-03-17 17:54:56 +0000

    Added new file to store some tests within it

--- tests.py
+++ tests.py
@@ -1,12 +1,15 @@
 import unittest
 
+from main import Block, Blockchain
+
 
 class BlockTests(unittest.TestCase):
     """
     Tests for the Block object
     """
     def test_block_properties_are_properly_created(self):
-        return self.fail("adda")
+        block = Block({'data': 'random'}, "0x1234")
+        return self.fail(block)
 
 if __name__ == "__main__":
     unittest.main()
