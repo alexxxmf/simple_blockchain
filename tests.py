da4e1994bc7b71bcaf544ba5271bd30208bdbb6a
alexxxmf <amoro@alumni.unav.es>
2018-03-17 17:53:02 +0000

    Added new file to store some tests within it

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BlockTests(unittest.TestCase):
+    """
+    Tests for the Block object
+    """
+    def test_block_properties_are_properly_created(self):
+        return self.fail("adda")
+
+if __name__ == "__main__":
+    unittest.main()
